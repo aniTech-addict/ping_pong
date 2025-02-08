bd9bf9fa4a9144631c295c1d7861c00cde444dba
Brad <kirti7666anand15@gmail.com>
2025-02-09 00:37:58 +0530

    Ball colision Game Over

--- ping_pong.py
+++ ping_pong.py
@@ -11,7 +11,7 @@ pygame.init()
 pygame.display.set_caption('Badminton Game')
 img = pygame.image.load('logo.jpg')
 pygame.display.set_icon(img)
-
+font = pygame.font.SysFont(None, 50)
 border = (800,600)
 # Define the dimensions of screen object
 screen = pygame.display.set_mode((border))
@@ -24,8 +24,11 @@ player1_score = 0
 player2_score = 0
 player1_pos = pygame.Vector2(30, screen.get_height()/2-50)
 player2_pos = pygame.Vector2(760, screen.get_height()/2-50)
-
-
+ball_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+#function
+def text_screen(text, color, x, y):
+    screen_print = font.render(text, True, color)
+    screen.blit(screen_print, (x, y))
 '''def is_inside(player, border):
     return (inner.left >= outer.left and
             inner.right <= outer.right and
@@ -45,6 +48,18 @@ while gameOn:
 			print(event)
 			gameOn = not gameOn
 
+		if (ball_pos.x > screen.get_width() or ball_pos.x < 0) or (ball_pos.y > screen.get_height() or ball_pos.y < 0):
+			game_on = not True
+			print("game over")
+			while not exit_game:
+				screen.fill("black")
+				text_screen("Game Over", "red", screen.get_width() / 2, screen.get_height() / 2)
+				text_screen("Player 1 : "+str(player1_score),"red",screen.get_width()/2,screen.get_height()/2+50)
+				text_screen("Player 2 : "+str(player2_score),"red",screen.get_width()/2,screen.get_height()/2+50)
+				pygame.display.update()
+				pygame.time.delay(5000)
+				exit_game = True
+
 	screen.fill((255, 255, 255))
 
 	#border
@@ -56,7 +71,7 @@ while gameOn:
 	pygame.draw.rect(screen,"blue", player1_rect,0)
 	pygame.draw.rect(screen,"green", player2_rect,0)
 
-	ball_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
+	
 	ball = pygame.draw.circle(screen,"red", (ball_pos.x, ball_pos.y), 10, 0)
 
 	# Movement Controls
